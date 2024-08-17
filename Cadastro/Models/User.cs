5882a7746cc1d026eaa6b34033c8897fd760da97
joaocss <joaocss@dcomp.ufs.br>
2024-08-16 23:00:18 -0300

    commit 3

---
+++ Cadastro/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Cadastro.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome não pode exceder 100 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Insira um e-mail válido.")]
+        [StringLength(100, ErrorMessage = "O e-mail não pode exceder 100 caracteres.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
+        [Phone(ErrorMessage = "Insira um número de telefone válido.")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
+
+        [StringLength(50, ErrorMessage = "O nome de usuário não pode exceder 50 caracteres.")]
+        public string NomeUsuario { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 100 caracteres.")]
+        public string Senha { get; set; }
+    }
+}
